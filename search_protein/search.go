573f9f7527246655ed5b49ee245feb2d5b850980
Guy Gadboit <guy@invalid.org>
2023-12-16 11:05:42 +0000

    Search for frameshifted proteins
    In human proteome

---
+++ search_protein/search.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"genomics/genomes"
+	"genomics/utils"
+	"io"
+	"log"
+	"strings"
+)
+
+/*
+	Look for len long substrings of needle in haystack
+*/
+func search(haystack *genomes.Genomes, needle []byte, length int) {
+	for start := 0; start < len(needle); start++ {
+		end := start + length
+		if end > len(needle) {
+			break
+		}
+
+		var s genomes.Search
+		for s.Init(haystack, 0,
+			needle[start:end], 0.0); !s.End(); s.Next() {
+			pos, _ := s.Get()
+			fmt.Printf("Found %s at %d (length %d)\n",
+				string(needle[start:end]), pos, length)
+		}
+	}
+}
+
+func main() {
+	gs := genomes.LoadGenomes(
+		"/fs/f/genomes/human/GRCh38_latest_protein.faa.gz", "", true)
+
+	for length := 7; length < 12; length++ {
+		f := utils.NewFileReader("peptides.txt")
+		defer f.Close()
+
+	loop:
+		for {
+			line, err := f.ReadString('\n')
+			switch err {
+			case io.EOF:
+				break loop
+			case nil:
+				break
+			default:
+				log.Fatal("Can't read peptides file")
+			}
+			line = strings.TrimSpace(line)
+			fmt.Printf("Searching %s length %d\n", line, length)
+			search(gs, []byte(line), length)
+		}
+	}
+}
