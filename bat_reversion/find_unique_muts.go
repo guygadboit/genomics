6400ddae1b3c3e57342501758a7dbffe5e49223b
Guy Gadboit <guy@invalid.org>
2024-03-27 15:08:20 +0000

    Look at SARS1
    And take all the actual variants of SARS1 in humans out of that
    relatives file.

--- bat_reversion/find_unique_muts.go
+++ bat_reversion/find_unique_muts.go
@@ -22,6 +22,27 @@ func checkUnique(alleles map[byte][]int,
 		"ORF10",
 	}
 
+	/*
+	For SARS1
+	names := []string{
+		"ORF1ab",
+		"ORF1ab",
+		"S",
+		"Unknown",
+		"Unknown",
+		"Unknown",
+		"Unknown",
+		"Unknown",
+		"Unknown",
+		"Unknown",
+		"Unknown",
+		"Unknown",
+		"Unknown",
+		"Unknown",
+		"Unknown",
+	}
+	*/
+
 	for k, v := range alleles {
 		if len(v) == 1 && len(alleles) == 2 {
 			var other byte
@@ -31,6 +52,12 @@ func checkUnique(alleles map[byte][]int,
 					break
 				}
 			}
+
+			// Don't show indels
+			if k == '-' || other == '-' {
+				continue
+			}
+
 			_, orf, pos := codon.OrfRelative(orfs)
 			fmt.Printf("Only in %d: %s: %c%d%c\n",
 				v[0], names[orf], k, pos/3+1, other)
@@ -41,6 +68,11 @@ func checkUnique(alleles map[byte][]int,
 func main() {
 	g := genomes.LoadGenomes("../fasta/SARS2-relatives.fasta",
 		"../fasta/WH1.orfs", false)
+
+	/*
+	g := genomes.LoadGenomes("../fasta/SARS1-only-relatives.fasta",
+		"../fasta/SARS1.orfs", false)
+	*/
 
 	translations := make([]genomes.Translation, g.NumGenomes())
 	for i := 0; i < g.NumGenomes(); i++ {
