a5e743d7e48d2308cf95c6660d6f55af5d154f58
Guy Gadboit <guy@invalid.org>
2024-10-31 14:28:29 +0000

    Just compare the viruses

--- peptide_similarity/peptide_similarity.go
+++ peptide_similarity/peptide_similarity.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"genomics/genomes"
+	"genomics/utils"
 )
 
 type PeptideSet map[string]bool
 
 func GetPeptides(virus *genomes.Genomes, merLen int) PeptideSet {
 	ret := make(PeptideSet)
-	for i := 0; i < virus.Length() - merLen; i++ {
-		prot := string(virus.Nts[0][i:i+merLen])
+	for i := 0; i < virus.Length()-merLen; i++ {
+		prot := string(virus.Nts[0][i : i+merLen])
 		ret[prot] = true
 	}
 	return ret
@@ -18,8 +19,8 @@ func GetPeptides(virus *genomes.Genomes, merLen int) PeptideSet {
 
 func Search(peptides PeptideSet, host *genomes.Genomes, merLen int) int {
 	ret := 0
-	for i := 0; i < host.Length() - merLen; i++ {
-		prot := string(host.Nts[0][i:i+merLen])
+	for i := 0; i < host.Length()-merLen; i++ {
+		prot := string(host.Nts[0][i : i+merLen])
 		if peptides[prot] {
 			ret++
 		}
@@ -42,20 +43,41 @@ func main() {
 		"SARS2",
 		"OC43",
 		"BANAL-20-52",
+		"RaTG13",
 		"SARS1",
 	}
 
+	getPeptides := func(name string, merLen int) PeptideSet {
+		vg := genomes.LoadGenomes(
+			root+fmt.Sprintf("viruses/%s/%s-prot.fasta.gz",
+				name, name), "", true)
+		return GetPeptides(vg, merLen)
+	}
+
+	for merLen := 5; merLen <= 9; merLen++ {
+		sc2 := getPeptides("SARS2", merLen)
+		for i, v := range viruses {
+			if i == 0 {
+				continue
+			}
+			peptides := getPeptides(v, merLen)
+			ix := len(utils.Intersection(sc2, peptides))
+			pct := float64(ix*100) / float64(len(peptides))
+			fmt.Printf("SC2 vs %s: %d/%d (%.2f%%) %d-mers in common\n",
+				v, ix, len(peptides), pct, merLen)
+		}
+	}
+	return
+
 	for merLen := 6; merLen <= 9; merLen++ {
 		for _, v := range viruses {
-			vg := genomes.LoadGenomes(
-				root+fmt.Sprintf("viruses/%s/%s-prot.fasta.gz", v, v), "", true)
-			peptides := GetPeptides(vg, merLen)
+			peptides := getPeptides(v, merLen)
 
 			for _, h := range hosts {
 				hg := genomes.LoadGenomes(
 					root+fmt.Sprintf("%s/%s-prot.fasta.gz", h, h), "", true)
-			count := Search(peptides, hg, merLen)
-			fmt.Printf("%d %s %s %d\n", merLen, v, h, count)
+				count := Search(peptides, hg, merLen)
+				fmt.Printf("%d %s %s %d\n", merLen, v, h, count)
 			}
 		}
 	}
