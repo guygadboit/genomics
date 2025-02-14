5cfd2cfde70fab2b3d8de672f5a43b92b2e36b7c
Guy Gadboit <guy@invalid.org>
2025-02-14 13:34:52 +0000

    Outdir option

--- tools/degpca/split_dat.py
+++ tools/degpca/split_dat.py
@@ -4,10 +4,11 @@ from pdb import set_trace as brk
 
 
 class Redirect:
-	def __init__(self, patterns, fname):
+	def __init__(self, outdir, patterns, fname):
+		self.outdir = outdir
 		self.patterns = patterns
 		self.fname = fname
-		self.fp = open(fname, "w")
+		self.fp = open("{}{}".format(outdir, fname), "w")
 
 	def _match(self, line):
 		for pat in self.patterns:
@@ -39,8 +40,12 @@ PATTERNS = (
 def main():
 	ap = ArgumentParser()
 	ap.add_argument("-t", "--title", type=str, default="Unknown")
+	ap.add_argument("-o", "--outdir", type=str, default="./")
 	ap.add_argument("fname", nargs=1)
 	args = ap.parse_args()
+
+	if args.outdir[-1] != '/':
+		args.outdir += '/'
 
 	redirects = []
 	for item in PATTERNS:
@@ -48,9 +53,10 @@ def main():
 			patterns, name = (item,), item
 		else:
 			patterns, name = item[0], item[1]
-		redirects.append(Redirect(patterns, name + ".dat"))
+		redirects.append(Redirect(args.outdir, patterns,
+				"{}.dat".format(name, ".dat")))
 
-	redirects.append(RedirectEverything((), "Others.dat"))
+	redirects.append(RedirectEverything(args.outdir, (), "Others.dat"))
 
 	with open(args.fname[0]) as fp:
 		for line in fp:
@@ -59,12 +65,12 @@ def main():
 				if r.handle(line):
 					break
 
-	with open("plot_split.gpi", "w") as fp:
+	with open("{}plot_split.gpi".format(args.outdir), "w") as fp:
 		names = ['"{}"'.format(r.fname) for r in redirects]
 		print('set title "{}"'.format(args.title), file=fp)
 		print("plot {}".format(", ".join(names)), file=fp)
 
-	print("Wrote plot_split.gpi")
+	print("Wrote {}plot_split.gpi".format(args.outdir))
 
 
 if __name__ == "__main__":
