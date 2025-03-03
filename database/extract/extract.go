2b68e0b1d7e0a971c1f15168128c01d71c34dbfd
Guy Gadboit <guy@invalid.org>
2025-03-03 07:46:34 +0000

    Extract optionally as a single MSA

--- database/extract/extract.go
+++ database/extract/extract.go
@@ -11,13 +11,19 @@ import (
 const ROOT = "/fs/f/genomes/viruses/SARS2/"
 
 func main() {
-	var reconstruct bool
-	var reference string
-	var orfs string
+	var (
+		reconstruct bool
+		msa         bool
+		reference   string
+		orfs        string
+	)
 
 	flag.BoolVar(&reconstruct, "reconstruct", false, "Reconstruct fasta files")
+	flag.BoolVar(&msa, "msa", false, "Output reconstruction of all inputs"+
+		"as a single multi-sequence alignment")
 	flag.StringVar(&reference, "ref", ROOT+"WH1.fasta", "Reference genome")
 	flag.StringVar(&orfs, "orfs", ROOT+"WH1.orfs", "Reference genome ORFs")
+
 	flag.Parse()
 
 	db := database.NewDatabase()
@@ -25,9 +31,12 @@ func main() {
 	// EPI_ISL_861438 is an example with some insertions and deletions you can
 	// test on.
 
-	var g *genomes.Genomes
+	var g, output *genomes.Genomes
 	if reconstruct {
 		g = genomes.LoadGenomes(reference, orfs, false)
+		if msa {
+			output = g.Filter(0)
+		}
 	}
 
 	for _, accNum := range flag.Args() {
@@ -47,9 +56,26 @@ func main() {
 				fmt.Fprintln(os.Stderr, err)
 				continue
 			}
-			name := fmt.Sprintf("%s.fasta", r.GisaidAccession)
-			rg.SaveMulti(name)
-			fmt.Printf("Wrote %s\n", name)
+
+			if !msa {
+				name := fmt.Sprintf("%s.fasta", r.GisaidAccession)
+				rg.SaveMulti(name)
+				fmt.Printf("Wrote %s\n", name)
+				continue
+			}
+
+			if len(rg.Nts[1]) != len(g.Nts[0]) {
+				continue
+			}
+
+			output.Nts = append(output.Nts, rg.Nts[1])
+			output.Names = append(output.Names, rg.Names[1])
 		}
+	}
+
+	if output != nil {
+		name := "GISAID-genomes.fasta"
+		output.SaveMulti(name)
+		fmt.Printf("Wrote %s\n", name)
 	}
 }
