c6622244a2a706561afb73496c39d9b5e53ad044
Guy Gadboit <guy@invalid.org>
2025-03-08 07:30:47 +0000

    OK this is now actually finding all of them
    Huge inconsistency in where people put the EPI name. Run this on a
    version of the xml on which you already did a grep -50 for EPI_ISL.

--- SantaCatarina/raw_reads.py
+++ SantaCatarina/raw_reads.py
@@ -1,6 +1,44 @@
 import gzip
+import sys
+import re
 from pdb import set_trace as brk
 import xml.etree.ElementTree as ET
+
+
+def save_sample(sample):
+	with open("debug.xml", "wt") as fp:
+		for line in sample:
+			print(line, file=fp)
+
+
+def grep(lines, pattern):
+	for line in lines:
+		if pattern in line:
+			return True
+	return False
+
+
+def search_desc(root):
+	# Look for the EPI_ISL bit in the Description
+	for desc in root.iter("Description"):
+		for element in desc.iter():
+			if element.text and "EPI_ISL" in element.text:
+				return element.text
+
+
+def search_attrs(root, attr_name):
+	# Look for the EPI_ISL bit in the Attributes
+	for atts in root.iter("Attributes"):
+		for att in atts.iter("Attribute"):
+			if attr_name in att.get("attribute_name").lower():
+				if att.text and "EPI_ISL" in att.text:
+					return att.text
+
+
+def clean_epi(epi_name):
+	pat = re.compile(r'(EPI_ISL_\d+)')
+	m = pat.search(epi_name)
+	return m.group(1)
 
 
 def output_sample(sample):
@@ -22,13 +60,15 @@ def output_sample(sample):
 
 	rr_name, epi_name, collection_date, location = None, None, None, None
 
-
 	for Id in root.iter("Id"):
 		a = Id.attrib
-		if a.get("is_primary") == "1":
+		if a.get("db") == "SRA":
 			rr_name = Id.text
+		elif a.get("is_primary") == "1":
+			if rr_name is None: rr_name = Id.text
 		elif a.get("db_label") == "Sample name":
-			epi_name = Id.text
+			if "EPI_ISL" in Id.text:
+				epi_name = Id.text
 
 	for At in root.iter("Attribute"):
 		a = At.attrib
@@ -37,7 +77,24 @@ def output_sample(sample):
 		elif a.get("attribute_name") == "geo_loc_name":
 			location = At.text
 
-		print(collection_date, location, epi_name, rr_name)
+	if epi_name is None:
+		epi_name = search_desc(root)
+
+	if epi_name is None:
+		for s in ("gisaid", "virus strain",
+				"sample name", "strain", "isolate",
+				"source_name", "passage_history", "note"):
+			epi_name = search_attrs(root, s)
+			if epi_name: break
+
+	if epi_name is not None:
+		epi_name = clean_epi(epi_name)
+
+# 	if grep(sample, "EPI_ISL") and epi_name is None:
+# 		save_sample(sample)
+# 		brk()
+
+	print(epi_name, rr_name, collection_date, location)
 
 
 def find_sras(fname):
@@ -56,7 +113,7 @@ def find_sras(fname):
 
 
 def main():
-	find_sras("/fs/f/tmp/biosample_set.xml.gz")
+	find_sras(sys.argv[1])
 
 
 if __name__ == "__main__":
