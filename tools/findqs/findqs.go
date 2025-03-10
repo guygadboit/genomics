73c4710f0c28a93a075500f1ec16601019b74188
Guy Gadboit <guy@invalid.org>
2025-03-10 07:26:18 +0000

    Show majority counts

--- tools/findqs/findqs.go
+++ tools/findqs/findqs.go
@@ -36,9 +36,28 @@ func printSorted(counts map[int]int) {
 	}
 }
 
+func majority(alleles map[byte]int) byte {
+	var ret byte
+	best := -1
+	for k, v := range alleles {
+		if v > best {
+			best = v
+			ret = k
+		}
+	}
+	return ret
+}
+
 func Compare(pileup *pileup.Pileup,
 	g *genomes.Genomes, minDepth int, requireSilent bool) {
 	counts := make(map[int]int)
+
+	// The total number of differences from g.Nts[0] with at least minDepth,
+	// and also silent if requireSilent.
+	diffs := 0
+
+	// The number of diffs that match the majority allele in the outgroup
+	totalMaj := 0
 
 	for i := 0; i < g.Length(); i++ {
 		rec := pileup.Get(i)
@@ -52,34 +71,47 @@ func Compare(pileup *pileup.Pileup,
 			if read.Depth <= minDepth {
 				continue
 			}
+			silent, _, _ := genomes.IsSilentWithReplacement(g,
+				i, 0, 0, []byte{read.Nt})
+			if requireSilent && !silent {
+				continue
+			}
 
+			diffs++
 			matches := make([]string, 0)
 			alleles := make(map[byte]int)
 
 			for j := 1; j < g.NumGenomes(); j++ {
 				alleles[g.Nts[j][i]]++
-				if requireSilent {
-					if silent, _, _ := genomes.IsSilentWithReplacement(g,
-						i, j, j, []byte{read.Nt}); !silent {
-						continue
-					}
-				}
 				if read.Nt == g.Nts[j][i] {
 					matches = append(matches, fmt.Sprintf("%d", j))
 					counts[j]++
 				}
 			}
 
-			if len(matches) != 0 {
-				fmt.Printf("%d%c depth:%d rank:%d matches:%s ", rec.Pos+1,
-					read.Nt, read.Depth, rank, strings.Join(matches, ","))
-				for k, v := range alleles {
-					fmt.Printf("%c:%d ", k, v)
-				}
-				fmt.Printf("\n")
+			var silentS string
+			if silent {
+				silentS = "*"
 			}
+
+			var majS string
+			if read.Nt == majority(alleles) {
+				majS = "M"
+				totalMaj++
+			}
+
+			fmt.Printf("%c%d%c%s%s depth:%d rank:%d matches:%d:%s ",
+				g.Nts[0][i], rec.Pos+1, read.Nt, silentS, majS, read.Depth,
+				rank, len(matches), strings.Join(matches, ","))
+
+			for k, v := range alleles {
+				fmt.Printf("%c:%d ", k, v)
+			}
+			fmt.Printf("\n")
 		}
 	}
+	rate := float64(totalMaj)/float64(diffs)
+	fmt.Printf("%d/%d %.2f are majority\n", totalMaj, diffs, rate)
 	printSorted(counts)
 }
 
