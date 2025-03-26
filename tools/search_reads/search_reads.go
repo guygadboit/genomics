f00e0d7c13dbc10c9154914227fb3b1b72f31b35
Guy Gadboit <guy@invalid.org>
2025-03-26 14:54:02 +0000

    Full alignment
    For this you use a reference and a range rather than a pattern

--- tools/search_reads/search_reads.go
+++ tools/search_reads/search_reads.go
@@ -25,15 +25,28 @@ func ParseFastq(fname string, cb func(name string, readData []byte)) {
 	})
 }
 
+func printHeading(heading string, char byte) {
+	fmt.Println(heading)
+	for range heading {
+		fmt.Printf("%c", char)
+	}
+	fmt.Printf("\n")
+}
+
 func showMatch(name string, pattern []byte, read []byte, pos int) {
-	fmt.Println(name)
 
 	n := len(pattern)
-	a := read[pos:pos+n]
+	a := read[pos : pos+n]
 
+	fmt.Println(name)
 	fmt.Println(string(read))
-	fmt.Println(string(a))
-	fmt.Println(string(pattern))
+	fmt.Println()
+
+	printHeading("Alignment of match", '=')
+	fmt.Println("Read    ", string(a))
+	fmt.Println("Pattern ", string(pattern))
+
+	fmt.Printf("         ")
 	for i := 0; i < len(a); i++ {
 		if a[i] == pattern[i] {
 			fmt.Printf("*")
@@ -44,17 +57,66 @@ func showMatch(name string, pattern []byte, read []byte, pos int) {
 	fmt.Printf("\n\n")
 }
 
+/*
+pos is the position in read where we found the pattern. refPos is where it is
+in the reference. Return the similarity.
+*/
+func showFullAlignment(read []byte, pos int,
+	ref *genomes.Genomes, refPos int) float64 {
+
+	printHeading("Alignment of whole read", '=')
+	var lineA, lineB, stars string
+	n := len(read)
+	matches := 0
+
+	for i, a := range read {
+		var b byte
+		j := (refPos - pos) + i
+		if j >= 0 && j < ref.Length() {
+			b = ref.Nts[0][j]
+		} else {
+			b = '-'
+		}
+		lineA += string(a)
+		lineB += string(b)
+		if a == b {
+			stars += "*"
+			matches++
+		} else {
+			stars += " "
+		}
+	}
+
+	for i := 0; i < n; i += 60 {
+		end := i + 60
+		if end > n {
+			end = n
+		}
+		fmt.Println("Read    ", lineA[i:end])
+		fmt.Println("Pattern ", lineB[i:end])
+		fmt.Println("        ", stars[i:end])
+		fmt.Println()
+	}
+	ss := float64(matches) / float64(n)
+	fmt.Printf("Similarity: %.2f\n", ss)
+	return ss
+}
+
 func main() {
 	var (
-		tol        float64
-		patS       string
-		verbose    bool
-		minMatches int
+		tol         float64
+		patS        string
+		verbose     bool
+		minMatches  int
+		refName     string
+		subseqRange string
 	)
 
 	flag.BoolVar(&verbose, "v", false, "Verbose")
 	flag.Float64Var(&tol, "tol", 0.1, "Tolerance")
 	flag.StringVar(&patS, "p", "", "Pattern to look for")
+	flag.StringVar(&refName, "ref", "", "Reference genome")
+	flag.StringVar(&subseqRange, "r", "", "Where to find pattern")
 	flag.IntVar(&minMatches, "m", 1, "Minimum number of matches")
 	flag.Parse()
 
@@ -63,9 +125,24 @@ func main() {
 		return
 	}
 
-	pattern := []byte(patS)
-	matches := 0
+	var pattern []byte
 
+	var start, end int
+	if subseqRange != "" {
+		ints := utils.ParseInts(subseqRange, ":")
+		start = ints[0] - 1
+		end = ints[1]
+	}
+
+	var ref *genomes.Genomes
+	if refName != "" {
+		ref = genomes.LoadGenomes(refName, "", false)
+		pattern = ref.Nts[0][start:end]
+	} else {
+		pattern = []byte(patS)
+	}
+
+	matches := 0
 	for _, fname := range flag.Args() {
 		ParseFastq(fname, func(name string, data []byte) {
 			var g genomes.Genomes
@@ -75,6 +152,9 @@ func main() {
 				pos, _ := search.Get()
 				if verbose {
 					showMatch(name, pattern, g.Nts[0], pos)
+					if ref != nil {
+						showFullAlignment(g.Nts[0], pos, ref, start)
+					}
 				}
 				matches++
 			}
@@ -84,6 +164,6 @@ func main() {
 		os.Exit(-1)
 	}
 	if verbose {
-		fmt.Println(matches)
+		fmt.Printf("%d reads match\n\n", matches)
 	}
 }
