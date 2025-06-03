2b807bc74300a03420c2e531f4a2c8cfdadf2462
Guy Gadboit <guy@invalid.org>
2025-06-03 16:44:45 +0100

    This looks better, but still doesn't match the online tool

--- codon_usage/codon_usage.go
+++ codon_usage/codon_usage.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"math"
 	"genomics/genomes"
 	"genomics/utils"
-	"strings"
 )
 
 type CodonFreq struct {
@@ -15,30 +16,40 @@ type CodonFreq struct {
 
 	// The "relative adaptiveness" is like a normalized version of the RSCU.
 	// The most preferred codon will end up as 1.0
-	RelAd            float64
+	RelAd float64
 }
 
-type CodonFreqTable map[string]CodonFreq
+type CodonFreqTable struct {
+	Frequencies map[string]CodonFreq
+	Optima	map[byte]float64	// The best RSCU for each aa
+}
 
-// What's the highest RSCU
-func (ct CodonFreqTable) Optimum(aa byte) float64 {
+// What's the highest RSCU for a particular AA?
+func (ct CodonFreqTable) CalcOptimum(aa byte) float64 {
 	syns := genomes.ReverseCodonTable[aa]
 	var best float64
 	for _, syn := range syns {
-		best = max(best, ct[syn].RSCU)
+		best = max(best, ct.Frequencies[syn].RSCU)
 	}
 	return best
 }
 
+func (ct *CodonFreqTable) UpdateOptima() {
+	ct.Optima = make(map[byte]float64)
+	for k, _ := range genomes.ReverseCodonTable {
+		ct.Optima[k] = ct.CalcOptimum(k)
+	}
+}
+
 // Update Aa, RSCU and RelAd for all the CodonFreqs in ct.
-func (ct CodonFreqTable) UpdateRSCUs() {
-	for k, v := range ct {
+func (ct *CodonFreqTable) UpdateComputedValues() {
+	for k, v := range ct.Frequencies {
 		aa := genomes.CodonTable[v.Codon]
 		syns := genomes.ReverseCodonTable[aa]
 
 		total := 0.0
 		for _, syn := range syns {
-			total += ct[syn].CountPer1000
+			total += ct.Frequencies[syn].CountPer1000
 		}
 
 		// What count per 1000 would we expect if all codons synonymous for
@@ -47,15 +58,25 @@ func (ct CodonFreqTable) UpdateRSCUs() {
 
 		// Then the RSCU is just what we do see over that expectation
 		rscu := v.CountPer1000 / expected
-		w := rscu / ct.Optimum(aa)
-		ct[k] = CodonFreq{v.Codon, v.CountPer1000, aa, rscu, w}
+		ct.Frequencies[k] = CodonFreq{v.Codon, v.CountPer1000, aa, rscu, 0}
+	}
+
+	// Once we know all the RSCUs we can work out the relative adaptations
+	ct.UpdateOptima()
+
+	// And now we can work out the relative adaptations
+	for k, v := range ct.Frequencies {
+		newCf := v
+		newCf.RelAd = v.RSCU / ct.Optima[v.Aa]
+		ct.Frequencies[k] = newCf
 	}
 }
 
 // Parse a table pasted out of e.g.
 // https://dnahive.fda.gov/dna.cgi?cmd=codon_usage&id=537&mode=tisspec
-func Parse(fname string) CodonFreqTable {
-	ret := make(CodonFreqTable)
+func Parse(fname string) *CodonFreqTable {
+	ret := new(CodonFreqTable)
+	ret.Frequencies = make(map[string]CodonFreq)
 	utils.Lines(fname, func(line string, err error) bool {
 		fields := strings.Split(line, "\t")
 		for i := 0; i < len(fields); i += 3 {
@@ -64,40 +85,51 @@ func Parse(fname string) CodonFreqTable {
 				continue
 			}
 			count := utils.Atof(strings.Trim(fields[i+1], " "))
-			ret[codon] = CodonFreq{codon, count, 0, 0, 0}
+			ret.Frequencies[codon] = CodonFreq{codon, count, 0, 0, 0}
 		}
 		return true
 	})
-	ret.UpdateRSCUs()
+	ret.UpdateComputedValues()
 	return ret
 }
 
-type CAICodon struct {
+type RelCodon struct {
 	genomes.Codon
-	CAI	float64		// How "preferred" this codon is compared to some reference
+	RelAd float64 // The relative adaptation of this codon
 }
 
-type CAITranslation []CAICodon
+// A translation decorated with relative adaptation values
+type RelTranslation []RelCodon
 
-func (c CAITranslation) Mean() float64 {
-	var total float64
-	for _, codon := range c {
-		total += codon.CAI
-	}
-	return total/float64(len(c))
-}
-
-func MakeCAITranslation(g *genomes.Genomes,
-	which int, ref CodonFreqTable) CAITranslation {
+func MakeRelTranslation(g *genomes.Genomes,
+	which int, ref *CodonFreqTable) (RelTranslation, float64) {
 	trans := genomes.Translate(g, which)
-	ret := make(CAITranslation, len(trans))
+	ret := make(RelTranslation, len(trans))
+
+	prodRSCU, prodBest := 1.0, 1.0
+	exceptions := 0
+
 	for i, codon := range trans {
-		cf := ref[codon.Nts]
-		best := ref.Optimum(codon.Aa)	// TODO cache these if you care
-		cai := cf.RSCU / best
-		ret[i] = CAICodon{codon, cai}
+		cf := ref.Frequencies[codon.Nts]
+		best := ref.Optima[codon.Aa]
+		ret[i] = RelCodon{codon, cf.RelAd}
+
+		prodRSCU *= cf.RSCU
+		prodBest *= best
+
+		switch codon.Nts {
+		case "ATG":
+			fallthrough
+		case "TGG":
+			exceptions++
+		}
 	}
-	return ret
+
+	l := float64(len(trans)-exceptions)
+	caiObs := math.Pow(prodRSCU, 1/l)
+	caiMax := math.Pow(prodBest, 1/l)
+
+	return ret, caiObs / caiMax
 }
 
 func main() {
@@ -106,8 +138,12 @@ func main() {
 		"../fasta/WH1.orfs", false)
 
 	g = g.Filter(0)
-	g.Truncate(21562, 25384)
+	// g.Truncate(21562, 25384)
 
-	caiTrans := MakeCAITranslation(g, 0, ref)
-	fmt.Println(caiTrans.Mean())
+	relTrans, cai := MakeRelTranslation(g, 0, ref)
+	fmt.Println("CAI", cai)
+
+	for _, rc := range relTrans {
+		fmt.Println(rc.RelAd)
+	}
 }
